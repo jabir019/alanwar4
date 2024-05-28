85c8c0da413f3ee4114dfb0a4bda997eb5a89843
ahmad jabir <jabir.alv019@gmail.com>
2024-05-28 20:02:05 +0700

    update day-2

--- src/pages/Pendaftaran.jsx
+++ src/pages/Pendaftaran.jsx
@@ -1,9 +1,34 @@
 import MainLayout from "../layouts/MainLayout";
+import Profil1 from "../assets/profil1.png";
 
 export default function Pendaftaran() {
   return (
     <>
-      <MainLayout>Pendaftaran</MainLayout>
+      <MainLayout>
+        <section className="section-profil">
+          <div className="container">
+            {/* bacground & overlay */}
+            <div className="bg overlay-bg"></div>
+            <img src={Profil1} className="bg img-bg" alt="profil1" />
+
+            <div className="row h-100 align-items-center justify-item-center ">
+              <div className="col-md-6 col-12 text-white align-self-center mt-5 ">
+                <h1>Pendaftaran</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div class="container mt-5">
+          <div class="row justify-content-center">
+            <div class="col-md-6">
+              <div class="colored-block">
+                <p>wkwkwkwkw</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MainLayout>
     </>
   );
 }
