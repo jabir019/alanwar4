4d60decbc9cd4d713213a8999cbd4b7f640b14bc
ahmad jabir <jabir.alv019@gmail.com>
2024-05-30 20:08:35 +0700

    up

--- src/layouts/SectionKegiatan.jsx
+++ src/layouts/SectionKegiatan.jsx
@@ -33,10 +33,7 @@ export default function SectionKegiatan() {
                   />
                   <div className="card-body">
                     <h5 className="card-title">{post.title.rendered}</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
+                    <p className="card-text">{post.date}</p>
                   </div>
                 </div>
               </div>
