7b344002e5fe2681d3bb2101445db7e1730b8044
ahmad jabir <jabir.alv019@gmail.com>
2024-05-30 19:48:06 +0700

    update content from wordpress

--- src/layouts/SectionAdmin.jsx
+++ src/layouts/SectionAdmin.jsx
@@ -13,12 +13,50 @@ export default function SectionAdmin() {
           </div>
         </div>
       </nav>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-6"></div>
+      <div className="container m-5 ">
+        <div className="row justify-content-center">
+          <div className="col-6 bg-angger">
+            <form>
+              <h2 className="text-center text-primary">
+                Membuat Postingan Website
+              </h2>
+              <div id="emailHelp" className="form-text">
+                Featured Media
+              </div>
+              <input type="file" className="form-control" />
+              <div className="mb-3">
+                <div id="emailHelp" className="form-text">
+                  Judul
+                </div>
+                <input
+                  type="username"
+                  className="form-control"
+                  id="username"
+                  placeholder="Judul Postingan"
+                />
+              </div>
+              <div className="mb-3">
+                <div id="emailHelp" className="form-text">
+                  Konten
+                </div>
+                <input
+                  type="username"
+                  className="form-control"
+                  id="username"
+                  placeholder="Konten Postingan"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary mb-2">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
+
+      <footer className="bg-primary text-center">
+        <p className="text-white">Created by Jabir</p>
+      </footer>
     </>
   );
 }
--- src/layouts/SectionKegiatan.jsx
+++ src/layouts/SectionKegiatan.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import kegiatan1 from "../assets/kegiatan1.png";
 import kegiatan2 from "../assets/kegiatan2.png";
 import kegiatan3 from "../assets/kegiatan3.png";
 
 export default function SectionKegiatan() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jabir.neuversity.site/wp-json/wp/v2/posts?")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
   return (
     <>
       <section>
@@ -11,42 +23,24 @@ export default function SectionKegiatan() {
         </div>
         <div className="container h-100 ">
           <div className="row mx-auto align-items-center justify-content-center ">
-            <div className="col-md-3 col-12 mb-3">
-              <div className="card">
-                <img src={kegiatan1} className="card-img-top" alt="kegiatan1" />
-                <div className="card-body">
-                  <h5 className="card-title">Belajar Web tanpa Coding</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
+            {posts.map((post) => (
+              <div className="col-md-3 col-12 mb-3">
+                <div className="card">
+                  <img
+                    src={kegiatan1}
+                    className="card-img-top"
+                    alt="kegiatan1"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{post.title.rendered}</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-3 col-12 mb-3">
-              <div className="card">
-                <img src={kegiatan2} className="card-img-top" alt="kegiatan2" />
-                <div className="card-body">
-                  <h5 className="card-title">Belajar Front end</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3 col-12 mb-3">
-              <div className="card">
-                <img src={kegiatan3} className="card-img-top" alt="kegiatan3" />
-                <div className="card-body">
-                  <h5 className="card-title">Belajar Coding</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
