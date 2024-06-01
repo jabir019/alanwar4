ace6e7fc49bea762630c873577dff57110b6d671
ahmad jabir <jabir.alv019@gmail.com>
2024-06-01 14:53:04 +0700

    upppp

--- src/components/CardProgram.jsx
+++ src/components/CardProgram.jsx
@@ -1,7 +1,7 @@
 export default function CardProgram(props) {
   return (
     <>
-      <div className="bg-white rounded-2 mb-3 p-4">
+      <div className="bg-white p-3 mb-4 rounded-3">
         <h3>{props.judul}</h3>
         <p>{props.deskripsi}</p>
       </div>
--- src/layouts/SectionProgram.jsx
+++ src/layouts/SectionProgram.jsx
@@ -3,51 +3,52 @@ import CardProgram from "../components/CardProgram";
 
 export default function SectionProgram() {
   return (
-    <>
-      <section className="container bg-primary-subtle py-5">
-        <div className="row">
-          <div className="col-md-4 col-12">
-            <h1 className="text-primary">
-              Program Unggulan Pondok Pesantren Al-Anwar IV
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              quae velit molestiae quam, eligendi eaque est maiores asperiores.
-              Incidunt, saepe?
-            </p>
-            <div>
-              <img className="w-100 mb-3" src={kegiatan} alt="kegiatan" />
-            </div>
-          </div>
-
-          <div className="col-md-4 col-12 mt-3 mt-md-0">
-            <CardProgram
-              judul="Tahfidz Alqur'an"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
-            <CardProgram
-              judul="Juz 'Amma"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
-            <CardProgram
-              judul="Kitab Kuning Bandongan"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
-            <CardProgram
-              judul="Metode Al-qur'an Yanbu'a"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
-            <CardProgram
-              judul="Metode Nahwu Al-Asas"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
-            <CardProgram
-              judul="Sorogan Kutubus Salaf"
-              deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
-            />
+    <section className="container bg-primary-subtle py-5">
+      <div className="row">
+        <div className="col-md-4 col-12">
+          <h1 className="text-primary">
+            Program Unggulan Pondok Pesantren Al-Anwar IV
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            quae velit molestiae quam, eligendi eaque est maiores asperiores.
+            Incidunt, saepe?
+          </p>
+          <div>
+            <img className="w-100 mb-3" src={kegiatan} alt="kegiatan" />
           </div>
         </div>
-      </section>
-    </>
+
+        <div className="col-md-4 col-12 mt-3 mt-md-0">
+          <CardProgram
+            judul="Tahfidz Alqur'an"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
+          />
+          <CardProgram
+            judul="Juz 'Amma"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
+          />
+          <CardProgram
+            judul="Kitab Kuning Bandongan"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam"
+          />
+        </div>
+
+        <div className="col-md-4 col-12 mt-3 mt-md-0">
+          <CardProgram
+            judul="Metode Alqur'an Yanbu'a"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit"
+          />
+          <CardProgram
+            judul="Metode Nahwu Al-Asas"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
+          />
+          <CardProgram
+            judul="Sorogan Kutubus Salaf"
+            deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae velit molestiae quam, eligendi eaque est maiores asperiores. Incidunt, saepe?"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
