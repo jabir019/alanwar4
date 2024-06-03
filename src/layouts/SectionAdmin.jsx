cb211eb560bc731264c6fe69847fe655b2d095b1
ahmad jabir <jabir.alv019@gmail.com>
2024-06-03 18:54:16 +0700

    push

--- src/layouts/SectionAdmin.jsx
+++ src/layouts/SectionAdmin.jsx
@@ -71,53 +71,55 @@ export default function SectionAdmin() {
   }
 
   return (
-    <div className="container my-5 ">
+    <div className="container">
       <div className="row justify-content-center">
-        <div className="col-6 bg-angger">
-          <form onSubmit={handleSubmit} encType="multipart/form-data">
-            <h2 className="text-center text-primary">
-              Membuat Postingan Website
-            </h2>
-            <div id="emailHelp" className="form-text">
-              Featured Media
-            </div>
-            <input
-              type="file"
-              className="form-control"
-              onChange={(e) => setImage(e.target.files[0])}
-              id="featured_media"
-            />
-            <div className="mb-3">
+        <div className="my-5 px-3 bg-angger">
+          <div className="col-12 col-md-8 m-auto">
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <h2 className="text-center text-primary py-3">
+                Membuat Postingan Website
+              </h2>
               <div id="emailHelp" className="form-text">
-                Judul
+                Featured Media
               </div>
               <input
-                value={judul}
-                onInput={(event) => setJudul(event.target.value)}
-                type="username"
+                type="file"
                 className="form-control"
-                id="judul"
-                rows={4}
-                placeholder="Judul Postingan"
+                onChange={(e) => setImage(e.target.files[0])}
+                id="featured_media"
               />
-            </div>
-            <div className="mb-3">
-              <div id="emailHelp" className="form-text">
-                Konten
+              <div className="mb-3">
+                <div id="emailHelp" className="form-text">
+                  Judul
+                </div>
+                <input
+                  value={judul}
+                  onInput={(event) => setJudul(event.target.value)}
+                  type="username"
+                  className="form-control"
+                  id="judul"
+                  rows={4}
+                  placeholder="Judul Postingan"
+                />
               </div>
-              <textarea
-                value={konten}
-                onInput={(event) => setKonten(event.target.value)}
-                type="username"
-                className="form-control"
-                id="username"
-                placeholder="Konten Postingan"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mb-2">
-              Submit
-            </button>
-          </form>
+              <div className="mb-3">
+                <div id="emailHelp" className="form-text">
+                  Konten
+                </div>
+                <textarea
+                  value={konten}
+                  onInput={(event) => setKonten(event.target.value)}
+                  type="username"
+                  className="form-control"
+                  id="username"
+                  placeholder="Konten Postingan"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary mb-2">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
