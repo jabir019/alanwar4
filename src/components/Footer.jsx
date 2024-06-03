adb61d747057c88464c95bf0d6077c839a2717a0
ahmad jabir <jabir.alv019@gmail.com>
2024-06-03 14:13:16 +0700

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,7 +47,7 @@ export default function Footer() {
           alt="anakhormat"
         />
 
-        <div className="bg-primary text-center m-0 ">
+        <div className="bg-primary text-center m-0 py-2 ">
           <div className="container">
             <p className="text-white py-2">Copyright ©️ 2024 by Jabir</p>
           </div>
